de098f978e546ad1241ffe35b69eea975540ed2f
vasiliyeskin <vasiliyeskin@yandex.ru>
2020-03-25 14:22:59 +0300

    Some changes

--- TestFreeFemPrograms/forMatlab/matlab_octave_3d_examples.m
+++ TestFreeFemPrograms/forMatlab/matlab_octave_3d_examples.m
@@ -30,32 +30,34 @@ Ey=ffreaddata('diffractionSInS3d_Ey.txt');
 % S3=[0.75 0.375 -0.5; ...
 %     0.375 0.75 -0.5];
 
-S1=[-15 0 -15;...
-    -15 -15 0];
-S2=[-15 0 15;...
-    -15 15 0];
-S3=[ 15 0 -15; ...
-     15 -15 0];
+boundOut = 3; 
+
+S1=[-boundOut 0 -boundOut;...
+    -boundOut -boundOut 0];
+S2=[-boundOut 0 boundOut;...
+    -boundOut boundOut 0];
+S3=[ boundOut 0 -boundOut; ...
+     boundOut -boundOut 0];
 
 figure(33);
 ffpdeplot3D(p,b,t,'VhSeq',vh,'XYZData',real(Ey),'Slice',S1,S2,S3,'SGridParam',[80,80],'BDLabels',[30,31], ...
             'XYZStyle','monochrome','ColorMap',jet(200),'ColorBar','on','BoundingBox','on');
 
-
-S1=[-1.5 0 -1.5];
-S2=[-1.5 0  1.5];
-S3=[ 1.5 0 -1.5];
-figure(35);
-ffpdeplot3D(p,b,t,'VhSeq',vh,'XYZData',real(Ey),'Slice',S1,S2,S3,'Project2D', 'on', ...
-            'Boundary','off','ColorMap',jet(200),'ColorBar','on');
-        
-        
-S1=[-1.5 -1.5 0];
-S2=[-1.5  1.5 0];
-S3=[ 1.5 -1.5 0];
-figure(36);
-ffpdeplot3D(p,b,t,'VhSeq',vh,'XYZData',real(Ey),'Slice',S1,S2,S3,'SGridParam',[50,50], 'Project2D', 'on', ...
-            'Boundary','off','ColorMap',jet(200),'ColorBar','on');
+% 
+% S1=[-1.5 0 -1.5];
+% S2=[-1.5 0  1.5];
+% S3=[ 1.5 0 -1.5];
+% figure(35);
+% ffpdeplot3D(p,b,t,'VhSeq',vh,'XYZData',real(Ey),'Slice',S1,S2,S3,'Project2D', 'on', ...
+%             'Boundary','off','ColorMap',jet(200),'ColorBar','on');
+%         
+%         
+% S1=[-1.5 -1.5 0];
+% S2=[-1.5  1.5 0];
+% S3=[ 1.5 -1.5 0];
+% figure(36);
+% ffpdeplot3D(p,b,t,'VhSeq',vh,'XYZData',real(Ey),'Slice',S1,S2,S3,'SGridParam',[50,50], 'Project2D', 'on', ...
+%             'Boundary','off','ColorMap',jet(200),'ColorBar','on');
 
         
         
